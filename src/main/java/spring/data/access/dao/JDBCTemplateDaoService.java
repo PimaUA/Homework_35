cb1d61e6a705a29df94af370fe549138605906b5
PimaUA <pimaua87@gmail.com>
2023-06-27 16:18:42 +0300

    init commit for FeatureBranch

--- src/main/java/spring/data/access/dao/JDBCTemplateDaoService.java
+++ src/main/java/spring/data/access/dao/JDBCTemplateDaoService.java
@@ -1,8 +1,20 @@
 package spring.data.access.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import spring.data.access.models.Product;
 
+import javax.sql.DataSource;
+
+@Repository
 public class JDBCTemplateDaoService implements DaoService{
+private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void init(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
 
     @Override
